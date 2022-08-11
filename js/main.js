3ec2853cff7cd9b52ffa52e8ab38a9aca05c8de1
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2022-08-11 09:25:11 +0700

    Fix Main JS

--- js/main.js
+++ js/main.js
@@ -35,61 +35,12 @@
     }
 
     /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true);
-    const navbarlinksActive = () => {
-        let position = window.scrollY + 200;
-        navbarlinks.forEach(navbarlink => {
-            if (!navbarlink.hash) return;
-            let section = select(navbarlink.hash);
-            if (!section) return;
-            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-                navbarlink.classList.add('active');
-            } else {
-                navbarlink.classList.remove('active');
-            }
-        })
-    }
-    window.addEventListener('load', navbarlinksActive);
-    onscroll(document, navbarlinksActive);
-
-    /**
      * Mobile nav toggle
      */
     on('click', '.mobile-nav-toggle', function (e) {
         select('#navbar').classList.toggle('navbar-mobile');
         this.classList.toggle('bi-list');
         this.classList.toggle('bi-x');
-    });
-
-    /**
-     * Scroll with offset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function (e) {
-        if (select(this.hash)) {
-            e.preventDefault();
-
-            let navbar = select('#navbar');
-            if (navbar.classList.contains('navbar-mobile')) {
-                navbar.classList.remove('navbar-mobile');
-                let navbarToggle = select('.mobile-nav-toggle');
-                navbarToggle.classList.toggle('bi-list');
-                navbarToggle.classList.toggle('bi-x');
-            }
-            scrollto(this.hash);
-        }
-    }, true);
-
-    /**
-     * Scroll with offset on page load with hash links in the url
-     */
-    window.addEventListener('load', () => {
-        if (window.location.hash) {
-            if (select(window.location.hash)) {
-                scrollto(window.location.hash);
-            }
-        }
     });
 
     on('click', '.test', function(e) {
